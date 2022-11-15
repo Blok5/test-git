b586db893930b65475718d87e001e824abbd9488
blok5 <Simakoff1z2x3c4v!>
2022-11-15 17:13:35 +0300

    first commit :)

---
+++ src/com/simakov/App.java
@@ -0,0 +1,23 @@
+package com.simakov;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        //git init
+
+        //git branch newTask
+        //
+
+        //git add .
+
+        //git commit -m "firstCommit"
+
+        //...
+
+        //git push
+
+        //git pull request (newTask -> master)
+        //git merge
+    }
+}
