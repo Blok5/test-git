c6ef2671b55123f74f2dde3743e36af88aa12531
Simakov Igor <Simakoff10@mail.ru>
2022-11-15 17:17:29 +0300

    some changes
    some changes

--- src/com/simakov/App.java
+++ src/com/simakov/App.java
@@ -2,22 +2,7 @@ package com.simakov;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
 
         //git init
-
-        //git branch newTask
-        //
-
-        //git add .
-
-        //git commit -m "firstCommit"
-
-        //...
-
-        //git push
-
-        //git pull request (newTask -> master)
-        //git merge
     }
 }
